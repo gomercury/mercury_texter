9e0b63ca5093cbb08a1264f18c75eca89fd90b98
Alexander Cheung <cheung.ying.lon@gmail.com>
2017-09-28 11:48:56 -0500

    correct status validation in text model

--- app/models/text.rb
+++ app/models/text.rb
@@ -3,7 +3,7 @@ class Text < ApplicationRecord
 	validates :to, presence: true, phone: { possible: true }
 	validates :from, presence: true, phone: { possible: true }
 	validates :body, presence: true
-	validates :status, null: false
+	validates :status, presence: true
 
 	# scopes
 	scope :pending, -> { where(status: "pending") }
